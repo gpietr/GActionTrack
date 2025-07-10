c3024582be39e6aae6ad801878bb119c0af85031
GrzegorzPietrzak <grzesiek.corp@gmail.com>
2025-07-10 22:56:15 +0200

    web display project

---
+++ GActionTrack.Web/Controllers/HomeController.fs
@@ -0,0 +1,37 @@
+﻿namespace GActionTrack.Web.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open System.Diagnostics
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+open GActionTrack
+
+open GActionTrack.Web.Models
+
+type HomeController (logger : ILogger<HomeController>) =
+    inherit Controller()
+
+    member this.Index () =
+        // For now, use the default in-memory storage and no filter
+        use storage = new Storage.LiteDbTrackingStorage("C:\\temp\\test-logs.db")
+        Queries.SetStorage(storage)
+        let sessions = Queries.GetSessions Queries.NoFilter
+        this.View(sessions)
+
+    member this.Privacy () =
+        this.View()
+
+    [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
+    member this.Error () =
+        let reqId = 
+            if isNull Activity.Current then
+                this.HttpContext.TraceIdentifier
+            else
+                Activity.Current.Id
+
+        this.View({ RequestId = reqId })
